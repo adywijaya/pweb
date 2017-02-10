30e78b9d687f3556b09526715416b27bf7ad1e48
Ady wijaya <adi.wijaya38@gmail.com>
2017-02-10 10:33:35 +0800

    tes table
    table poin

--- application/views/poin.php
+++ application/views/poin.php
@@ -1,25 +1,24 @@
 <section id="services">
-
 <div class="container">
-<div class="col-md-10 col-md-push-1">
-  <center><h1>Informasi Anggota</h1><center>
-  <br><br>
+  <div class="col-md-10 col-md-push-1">
+    <center><h1>Informasi Anggota</h1><center>
+    <br><br>
+    <div class="table-responsive">
+    <table class="table table-striped table-hover">
+      <thead>
+        <tr>
+          <th>No</th>
+          <th>NIM</th>
+          <th>Nama</th>
+          <th>Jurusan</th>
+          <th>Jenis Kelamin</th>
+          <th>Email</th>
+          <th>Angkatan</th>
+          <th>Point</th>
+        </tr>
+      </thead>
 
-<table class="table table-striped">
-    <thead>
-    <tr>
-      <th>No</th>
-      <th>NIM</th>
-      <th>Nama</th>
-      <th>Jurusan</th>
-      <th>Jenis Kelamin</th>
-      <th>Email</th>
-      <th>Angkatan</th>
-      <th>Point</th>
-    </tr>
-    </thead>
-
-    <tbody>
+      <tbody>
       <?php $no = 1; foreach ($users as $u) { ?>
           <tr>
             <td><?php echo $no++ ?></td>
@@ -33,8 +32,9 @@
           </tr>
        <?php } ?>
 
-    </tbody>
+      </tbody>
     </table>
+    </div>
   </div>
 </div>
 </section>
